18687aa2519f1d2f4234c5be2cdbc7c6e8f17580
upatricck <patrickuwonkunda@gmail.com>
2017-02-06 23:10:41 +0200

    added functions to do calculations

---
+++ primes.py
@@ -0,0 +1,24 @@
+
+
+def prime_numbers(n):
+   """
+   A function that returns prime numbers in range
+   0 to n
+   """
+   prime_numbers = []
+   for num in range(2, n):
+       if is_prime(num):
+           prime_numbers.append(num)
+  
+   return prime_numbers
+
+def is_prime(num):
+   """
+   Return true if a number is a 
+   prime number
+   """
+   for i in range(2, num-1):
+       if num % i == 0:
+           return False
+
+   return True
