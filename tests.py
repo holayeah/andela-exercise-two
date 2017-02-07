df23240f82bd880cde14dc75b49ab5f3bbc7792b
upatricck <patrickuwonkunda@gmail.com>
2017-02-07 08:53:20 +0200

    fixing syntax errors in tests

--- tests.py
+++ tests.py
@@ -4,19 +4,19 @@ from primes import prime_numbers, is_prime
 class PrimeTestCase(unittest.TestCase):
 
     def test_input_zero(self):
-        self.assertRaises(Exception):
+        with self.assertRaises(Exception):
             prime_numbers(0)
 
     def test_input_one(self):
-        self.assertRaises(Exception):
+        with self.assertRaises(Exception):
             prime_numbers(1) # The function can not output between range 0, 1
 
     def test_is_not_negative(self):
-        self.assertRaises(ValueError):
+        with self.assertRaises(ValueError):
             prime_numbers(-2)
 
     def test_is_an_instance_of_int(self):
-        self.assertRaises(TypeError):
+        with self.assertRaises(TypeError):
             prime_numbers("string")
 
     def test_getting_primes_list(self):
@@ -37,5 +37,5 @@ class PrimeTestCase(unittest.TestCase):
         list_returned = prime_numbers(1000)
         self.assertEqual(len(list_returned), 168)
 
-if __name__ == '__main__'
+if __name__ == '__main__':
     unittest.main()
